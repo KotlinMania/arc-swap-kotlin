5fa883d21fb787549532c9f1c0cf043ebb37c6cf
Sydney Renee <sydney@solace.ofharmony.ai>
2026-06-05 18:25:53 -0700

    chore: integrate new build script template

--- build.gradle.kts
+++ build.gradle.kts
@@ -209,6 +209,7 @@ kotlin {
     compilerOptions {
         allWarningsAsErrors.set(true)
         freeCompilerArgs.add("-Xexpect-actual-classes")
+        freeCompilerArgs.add("-Xsuppress-version-warnings")
     }
 
     val xcf = XCFramework("ArcSwap")
